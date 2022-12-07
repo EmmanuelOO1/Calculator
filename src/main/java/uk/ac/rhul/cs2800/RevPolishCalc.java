62c065c8bb48bb636fbee174108db985bbbd7ef2
Emmanuel Okolie <emmanuelokolie@Emmanuels-MacBook-Air.local>
2022-12-07 16:13:09 +0000

    Addition in RPC and Part of the way through the other operators

--- src/main/java/uk/ac/rhul/cs2800/RevPolishCalc.java
+++ src/main/java/uk/ac/rhul/cs2800/RevPolishCalc.java
@@ -1,23 +1,66 @@
 package uk.ac.rhul.cs2800;
+
 import java.util.Scanner;
 
 public class RevPolishCalc {
   NumStack values = new NumStack();
 
   public float evaluate(String string) {
-    Scanner myScanner = new Scanner(string);  // Create a Scanner object
+    Scanner myScanner = new Scanner(string); // Create a Scanner object
+
+  //  while (myScanner.hasNext()) {
+  //    if (myScanner.hasNextFloat()) {
+  //      values.push(myScanner.nextFloat());
+  //      continue;
+
+   //     }
+      
+      
+    float value1;
+    float value2;
     
-    while(myScanner.hasNext()) { 
-      if(myScanner.hasNextFloat()) {
-        values.push(myScanner.nextFloat());
-        continue;
+    for(int i = 0; i < string.length(); i++) {
+      switch(string(i)) {
+        case"+":
+          value1 = values.pop();
+          value2 = values.pop();
+          values.push(value2 + value1);
+          break;
+          
+        case"-":
+            value1 = values.pop();
+            value2 = values.pop();
+            values.push(value2 - value1);
+          break;
+          
+        case"*":
+          value1 = values.pop();
+          value2 = values.pop();
+          values.push(value2 * value1);
+          break;
+          
+        case"/":
+          value1 = values.pop();
+          value2 = values.pop();
+          values.push(value2 / value1);
+          break;
+          
+        default:
+          values.push(Double.parseDouble(string(i)));
+          
+          
+        
+      }
       }
     }
-    float num1 = values.pop();
-    float num2 = values.pop();
-    values.push(num1 + num2);
-    
-    return values.pop();  // Output user input
+
+
+
+    //float num1 = values.pop();
+    //float num2 = values.pop();
+    //values.push(num1 + num2);
+
+    return values.pop(); // Output user input
   }
 
 }
--- src/test/java/uk/ac/rhul/cs2800/TestRevPolishCalc.java
+++ src/test/java/uk/ac/rhul/cs2800/TestRevPolishCalc.java
@@ -1,7 +1,6 @@
 package uk.ac.rhul.cs2800;
 
-import static org.junit.jupiter.api.Assertions.*;
-import java.util.EmptyStackException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -12,23 +11,28 @@ class TestRevPolishCalc {
   public void setup() {
     values = new RevPolishCalc();
   }
-  
-  //1
+
+  // 1
   // RevPolishCalc should be able to add two positive numbers
   @Test
   void testAddPositive() {
     assertEquals(values.evaluate("2  3 + "), 5.0f, "2+ 3 should be 5");
   }
-  
-  //2
+
+  // 2
   // RevPolishCalc should be able to add two nevative numbers.
   @Test
   void testAddNegative() {
-    assertEquals(values.evaluate("-2  -3 + "), -5.0f, "2+ 3 should be 5");
+    assertEquals(values.evaluate("-2  -3 + "), -5.0f, "-3+ -2 should be -5");
   }
-  
-  
-  
-  
+
+  // 3
+  // RevPolishCalc should be able to subtract two numbers.
+  @Test
+  void testSubtract() {
+    assertEquals(values.evaluate("2  3 - "), 1.0f, "3 - 2 should be 1");
+  }
+
+
 
 }
