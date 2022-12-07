6f5e18943f21a824add45786473b290a65ad0f63
Emmanuel Okolie <emmanuelokolie@Emmanuels-MacBook-Air.local>
2022-12-07 18:11:45 +0000

    Javadoc and Checkstyle corrections

--- src/main/java/uk/ac/rhul/cs2800/RevPolishCalc.java
+++ src/main/java/uk/ac/rhul/cs2800/RevPolishCalc.java
@@ -1,72 +1,60 @@
 package uk.ac.rhul.cs2800;
 
-//import java.util.Scanner;
-
+/**
+ *My RevPolish Calculator Class.
+ *
+ * @author emmanuelokolie
+ *
+ */
 public class RevPolishCalc {
   NumStack values = new NumStack();
 
+  /**
+   *Function which evaluates expressions handed to it in string form.
+   *
+   * @param string of numbers and operands
+   *
+   * @return The value at the end of the calculation
+   */
   public float evaluate(String string) {
-//    Scanner myScanner = new Scanner(string); // Create a Scanner object
-//
-//  //  while (myScanner.hasNext()) {
-//  //    if (myScanner.hasNextFloat()) {
-//  //      values.push(myScanner.nextFloat());
-//  //      continue;
-//
-//   //     }
-//      
-//    
-//    Scanner scanner = new Scanner(string);  // Create a Scanner object
-//    String userName = scanner.nextLine();  // Read user input
-//    System.out.println("Username is: " );  // Output user input
-      
+
+    String[] numSymbols = string.split(" ");
     float value1;
     float value2;
-    
-    String[] numSymbols = string.split(" ");
-        
-    for(int i = 0; i < numSymbols.length; i++) {
-      switch(numSymbols[i]) {
-        case"+":
+
+
+    for (int i = 0; i < numSymbols.length; i++) {
+      switch (numSymbols[i]) {
+        case "+":
           value1 = values.pop();
           value2 = values.pop();
           values.push(value2 + value1);
           break;
-          
-        case"-":
-            value1 = values.pop();
-            value2 = values.pop();
-            values.push(value2 - value1);
+
+        case "-":
+          value1 = values.pop();
+          value2 = values.pop();
+          values.push(value2 - value1);
           break;
-          
-        case"*":
+
+        case "*":
           value1 = values.pop();
           value2 = values.pop();
           values.push(value2 * value1);
           break;
-          
-        case"/":
+
+        case "/":
           value1 = values.pop();
           value2 = values.pop();
           values.push(value2 / value1);
           break;
-          
+
         default:
           values.push(Float.parseFloat(numSymbols[i]));
-          
-          
-        
       }
-      
     }
 
-
-
-    //float num1 = values.pop();
-    //float num2 = values.pop();
-    //values.push(num1 + num2);
-
-    return values.pop(); // Output user input
+    return values.pop();
   }
 
 }
--- src/test/java/uk/ac/rhul/cs2800/TestRevPolishCalc.java
+++ src/test/java/uk/ac/rhul/cs2800/TestRevPolishCalc.java
@@ -1,8 +1,11 @@
 package uk.ac.rhul.cs2800;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+
 
 class TestRevPolishCalc {
   private RevPolishCalc values;
@@ -31,6 +34,35 @@ class TestRevPolishCalc {
   @Test
   void testSubtract() {
     assertEquals(values.evaluate("2 3 -"), -1.0f, "3 - 2 should be 1");
+  }
+
+  // 4
+  // RevPolishCalc should be able to times two numbers.
+  @Test
+  void testTimes() {
+    assertEquals(values.evaluate("6 3 *"), 18.0f, "6 * 3 should be 18");
+  }
+
+  // 5
+  // RevPolishCalc should be able to times two numbers.
+  @Test
+  void testTimesNegative() {
+    assertEquals(values.evaluate("6 -3 *"), -18.0f, "6 * -3 should be -18");
+  }
+
+
+  // 6
+  // RevPolishCalc should be able to divide two numbers.
+  @Test
+  void testDivide() {
+    assertEquals(values.evaluate("6 2 /"), 3.0f, "6 / 2 should be 3");
+  }
+
+  // 7
+  // RevPolishCalc should be able to divide two negative numbers.
+  @Test
+  void testDivideNegative() {
+    assertEquals(values.evaluate("6 -3 /"), -2f, "6 * -3 should be -2");
   }
 
 
