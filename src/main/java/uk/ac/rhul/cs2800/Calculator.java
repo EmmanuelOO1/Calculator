6680715bb0f11109f5d5ad3c40506915551db5a2
Emmanuel Okolie <emmanuelokolie@Emmanuels-MacBook-Air.local>
2022-12-07 19:07:28 +0000

    javadoc and Checkstyle

--- src/main/java/uk/ac/rhul/cs2800/Calculator.java
+++ src/main/java/uk/ac/rhul/cs2800/Calculator.java
@@ -1,6 +1,12 @@
 package uk.ac.rhul.cs2800;
 
 
+/**
+ *My Calculator interface.
+ *
+ * @author emmanuelokolie
+ *
+ */
 interface Calculator {
   double evaluate(String string); // interface method (does not have a body)
 }
