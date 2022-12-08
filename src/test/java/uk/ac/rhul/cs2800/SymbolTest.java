ae638e61edf4994c3976cfdc601e60d054a8f137
Emmanuel Okolie <emmanuelokolie@Emmanuels-MacBook-Air.local>
2022-12-08 21:18:00 +0000

    Symbol Edits

--- src/test/java/uk/ac/rhul/cs2800/SymbolTest.java
+++ src/test/java/uk/ac/rhul/cs2800/SymbolTest.java
@@ -10,37 +10,37 @@ class SymbolTest {
 	//Test 1
 	@Test
 	void testLEFT_BRACKET() {
-		assertEquals(Symbol.LEFT_BRACKET.toString(), "LEFT_BRACKET");	
+		assertEquals(Symbol.LEFT_BRACKET.toString(), "(");	
 	}
 	
 	//Test 2
 	@Test
 	void testRIGHT_BRACKET() {
-		assertEquals(Symbol.RIGHT_BRACKET.toString(), "RIGHT_BRACKET");	
+		assertEquals(Symbol.RIGHT_BRACKET.toString(), ")");	
 	}
 	
 	//Test 3
 	@Test
 	void testTIMES() {
-		assertEquals(Symbol.TIMES.toString(), "TIMES");	
+		assertEquals(Symbol.TIMES.toString(), "*");	
 	}
 	
 	//Test 4
 	@Test
 	void testDIVIDE() {
-		assertEquals(Symbol.DIVIDE.toString(), "DIVIDE");	
+		assertEquals(Symbol.DIVIDE.toString(), "/");	
 	}
 	
 	//Test 5
 	@Test
 	void testPLUS() {
-		assertEquals(Symbol.PLUS.toString(), "PLUS");	
+		assertEquals(Symbol.PLUS.toString(), "+");	
 	}
 	
 	//Test 6
 	@Test
 	void testMINUS() {
-		assertEquals(Symbol.MINUS.toString(), "MINUS");	
+		assertEquals(Symbol.MINUS.toString(), "-");	
 	}
 
 	//Test 7
